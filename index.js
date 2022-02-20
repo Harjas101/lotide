8bbcf246c6435e0ebb09c757013e48d48595f143
Harjas Anand <anand.harjas@gmail.com>
2022-02-20 09:58:00 -0500

    implement all functions in index

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const head   = require('./head');
 const tail   = require('./tail');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findkeybyvalue');
+const letterPositions = require('./letterPositions');
 const middle = require('./middle');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
-module.exports = {head, tail, middle};
+module.exports = {head, tail, middle, without, takeUntil, letterPositions, findKeyByValue, eqObjects, eqArrays, countOnly, countLetters, assertObjectsEqual, assertEqual, assertArraysEqual};
