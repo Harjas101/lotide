902065115965c0570d90a686c3346a7e00939b7a
Harjas101 <anand.harjas@gmail.com>
2022-03-08 19:57:43 -0500

    removed the files that did not need to be there for evaluation

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,5 @@
 const middle = require('../middle')
 const assert = require('chai').assert;
-
 describe("#middle", () => {
   it("returns [2] for [1, 2, 3]", () => {
     console.log(middle([1, 2, 3]))
